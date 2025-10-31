0abbce69ee934c617491212440251db53b38064d
ckacy01 <ckacy01@protonmail.com>
2025-10-31 15:28:25 -0600

    feat: Adding ManorResponse model

---
+++ src/main/java/org/technoready/escaperoom/model/ManorResponse.java
@@ -0,0 +1,18 @@
+package org.technoready.escaperoom.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+class ManorResponse {
+    private String narrative;
+    private String status;
+    private String inscription;
+    private Integer currentStage;
+}
