d1a34bc4eb0f493665fb4a376d50465d0d0980b6
ckacy01 <ckacy01@protonmail.com>
2025-10-31 16:34:14 -0600

    fix: Swagger documentation

---
+++ src/main/java/org/technoready/escaperoom/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package org.technoready.escaperoom.config;
+
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .version("1.0.0")
+                        .description("REST API for escape room")
+                        .contact(new Contact()
+                                .name("ckacy01")
+                                .email("it.jorgeavila@gmail.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")));
+    }
+}
